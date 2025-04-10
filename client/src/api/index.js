dadde934a4dfd38a3a452077243d10b8f7412309
saloni-gupta27 <guptasaloni.2016@gmail.com>
2025-04-10 18:42:00 +0530

    changing api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API= axios.create({
-    baseURL:"http://localhost:8080/api",
+    // baseURL:"http://localhost:8080/api",
+    baseURL:"https://imagegenerator-ypik.onrender.com/api"
 })
 export const GetPost = async ()=>{
     return await API.get("/post/")
